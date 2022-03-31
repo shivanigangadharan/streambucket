a63fcae8f0c8f4a2b52ffcbcc1cf2fae8cfb9d63
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-03-31 22:52:42 +0530

    videolisting page done

--- src/pages/videolisting/videolisting.js
+++ src/pages/videolisting/videolisting.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './videolisting.css';
+import Videocard from '../../components/videocard/videocard';
 
 export default function Videolisting() {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
     return (
-        <div>
-            <h1> Trending videos - Videolisting </h1>
+        <div className="videolisting-container">
+            <h1 className="title"> Trending videos </h1>
+            <div className="video-grid">
+                {
+                    arr.map((e) => {
+                        return <Videocard />
+                    })
+                }
+            </div>
         </div>
     )
 }
