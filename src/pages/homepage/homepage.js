178739fc4a83d36d2d1df43fba50185f55686b89
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-04-01 02:25:35 +0530

    Screen: Homepage done

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './homepage.css';
+import Videocard from '../../components/videocard/videocard';
+import { Link } from 'react-router-dom';
 
 export default function Homepage() {
     return (
-        <div>
-            <h1> Homepage </h1>
+        <div className="homepage-container">
+            <div className="banner">
+                <div className="banner-title"> STREAM BUCKET</div>
+                <div className="banner-subtext"> Watch HD videos, create playlists, share with your friends and more.</div>
+                <Link to="/videolisting">
+                    <button className="btn start"> START WATCHING NOW </button>
+                </Link>
+            </div>
+
+            <h3 className="bold"> Must watch videos  </h3>
+
+            <div className="video-grid">
+                <Videocard /><Videocard /><Videocard />
+                <Videocard />
+
+            </div>
+
         </div>
     )
 }
